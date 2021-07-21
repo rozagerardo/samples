c18f7dee7b909309649cdb746bf6fe593c4b6ac1
Gerardo Roza <rozagerardo@gmail.com>
2021-07-21 17:17:32 -0300

    initial commit

---
+++ sample-cosium/src/main/java/com/sample/MyClass.java
@@ -0,0 +1,33 @@
+package com.sample;
+
+import javax.persistence.EntityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootApplication
+public class MyClass implements ApplicationRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MyClass.class);
+
+	@Autowired
+	EntityManager entityManager;
+
+	public static void main(final String... args) {
+		SpringApplication.run(MyClass.class, args);
+	}
+
+	@Override
+	@Transactional
+	public void run(ApplicationArguments args) throws Exception {
+
+		LOG.info("actual change");
+
+		LOG.info("Existing statement");
+	}
+}
